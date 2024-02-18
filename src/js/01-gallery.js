b27992111215e1cbb012a0683533918adfcbbd79
d3monw1ngs <tezzielumsit87@gmail.com>
2024-02-19 00:24:17 +0800

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+    let li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+    </li>
+    `;
+    gallery.innerHTML += li;
+});
+
