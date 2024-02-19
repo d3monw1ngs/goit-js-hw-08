155fa03d517b7fd43f92afcaee87db2d0f79186f
d3monw1ngs <tezzielumsit87@gmail.com>
2024-02-20 00:10:35 +0800

    update task 2 and 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector(".feedback-form");
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const saveFormState = throttle(() => {
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+    const storedState = localStorage.getItem('feedback-form-state');
+
+    if (storedState) {
+        const parsedState =  JSON.parse(storedState);
+        emailInput.value = parsedState.email;
+        messageInput.value = parsedState.message;
+    }
+};
+
+const submitForm = (e) => {
+    e.preventDefault();
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+
+
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log("Form Data:", formState);
+};
+
+feedbackForm.addEventListener('input', () => {
+    saveFormState();
+});
+
+window.addEventListener('load', () => {
+    loadFormState();
+});
+
+feedbackForm.addEventListener('submit', submitForm);
 
